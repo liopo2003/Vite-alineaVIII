2cc5994d6d847a206265bf68a9d548196c29b8a2
leopoldo landini <leopoldolandini@hotmail.com>
2023-04-13 20:50:17 -0300

    once

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, RouterProvider, createBrowserRouter, NavLink } from 'react-router-dom'
+import { Outlet, RouterProvider, createHashRouter } from 'react-router-dom'
 import ErrorPage from "../Page/error-page";
 import Pagina2 from "../Page/Pagina2";
 import Pagina1 from "../Page/Pagina1";
@@ -9,7 +9,7 @@ import Sidebar from './SideBar';
 
 export default function Layout() {
 
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: (
